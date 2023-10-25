e51818523e61567d2433b21dc4a263f688df84be
rersand <134522744+AndreRuanSilva@users.noreply.github.com>
2023-10-25 11:37:06 -0300

    tecla clap e ajustes tecla pom

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-alert('olá mundo')
-function tocaSomPom(){
+function TocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
+ 
 
+function TocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = TocaSomClap;
