5cd66cc16a3950ba2701cf0fd23e408cd63a89b4
rersand <134522744+AndreRuanSilva@users.noreply.github.com>
2023-10-25 11:45:08 -0300

    ajuste tecla tim

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ document.querySelector('.tecla_clap').onclick = TocaSomClap;
 
 
 function TocaSomTim(){
-    document.querySelector('#som_tecla_tim').play()
+    document.querySelector('#som_tecla_tim').play();
 }
 document.querySelector('.tecla_tim').onclick = TocaSomTim;
